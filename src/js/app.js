a4d68fdff8c07aa39ba46f1fe7e65c4bf9e644d9
fortavey <fortavey@gmail.com>
2020-12-17 20:31:49 +0200

    17.12.2020

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,42 @@ try{
         });
     })();
 }catch(err){console.log(err);}
+
+// jQuery
+try{
+    jQuery(function($){
+        $('.reviews .container').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: $('.arrows .prev'),
+            nextArrow: $('.arrows .next'),
+            responsive: [
+                {
+                  breakpoint: 900,
+                  settings: {
+                    slidesToShow: 2
+                  }
+                },
+                {
+                  breakpoint: 700,
+                  settings: {
+                    slidesToShow: 1
+                  }
+                }
+            ]
+        });
+    });
+}catch(err){console.log(err)}
+
+// Open review
+try{
+   (function(){
+       const btn = document.querySelector('.reviews .container');
+        btn.addEventListener('click', e => {
+            console.log(e.target);
+            if(e.target.classList.contains('open') || e.target.classList.contains('arrow')) {
+                e.target.closest('.text').classList.toggle('opened');
+            }
+        });
+   })();
+}catch(err){console.log(err)}
